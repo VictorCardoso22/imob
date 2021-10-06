0135ab960426ea0b3ca774c2c276a944a41db1bb
VictorCardoso22 <victor.eacd@gmail.com>
2021-10-05 21:29:38 -0300

    Galeria de imagens (Front)

--- front/registro.php
+++ front/registro.php
@@ -1,1 +1,30 @@
 <?php include("header.php"); ?>
+
+<div class="container">
+  <div class="titulo">
+    <h2> Registros: </h2>
+  </div>
+
+  
+  <div class="photo-gallery">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Galeria de imagens</h2>
+                
+            </div>
+            <div class="row photos">
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="img/logo1.png" data-lightbox="photos"><img class="img-fluid" src="img/logo1.png"></a></div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="img/logo2.png" data-lightbox="photos"><img class="img-fluid" src="img/logo2.png"></a></div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="img/logo3.png" data-lightbox="photos"><img class="img-fluid" src="img/logo3.png"></a></div>
+            </div>
+        </div>
+    </div>
+    
+
+
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
